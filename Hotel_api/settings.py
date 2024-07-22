2c7a091ce70a78fa21e9af7b47a74f77a11a5537
RicardoPilarte <hotelometepe@gmail.com>
2024-07-22 09:42:58 -0600

    REDIS_URL

--- Hotel_api/settings.py
+++ Hotel_api/settings.py
@@ -115,7 +115,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_PUBLIC_URL'))],
+            "hosts": [(os.environ.get('REDIS_URL'))],
 
         },
     },
